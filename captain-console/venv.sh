107d6585af73f0c53916996a1630a359ed5ead70
ornstrange <orn.strange@gmail.com>
2020-04-27 15:11:09 +0000

    final version... hopefully

--- captain-console/venv.sh
+++ captain-console/venv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-[[ -d ".env" ]] && source .env/bin/activate || virtualenv .env ; source .env/bin/activate
+[[ -d ".env" ]] && source .env/bin/activate || virtualenv .env ; source .env/bin/activate ; pip install -r requirements.txt
 
