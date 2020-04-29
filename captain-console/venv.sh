e43313f77363dc24e9557e8cec5a8634821119b5
ornstrange <orn.strange@gmail.com>
2020-04-29 15:25:30 +0000

    fix venv.sh

--- captain-console/venv.sh
+++ captain-console/venv.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# set correct path based on os
+[[ $OSTYPE =~ "(darwin|linux-gnu)" ]] && path=".env/bin/activate" || path=".env/scripts/activate"
+
+# create virtual environment, activate and install dependencies if run for first time, else activate it
 [[ -d ".env" ]] && source .env/bin/activate || virtualenv .env ; source .env/bin/activate ; pip install -r requirements.txt
 
