2ed7cb3e4ad61fa58cef43de2ed93938f9232451
ornstrange <orn.strange@gmail.com>
2020-04-30 14:08:12 +0000

    final venv script... ég lofa

--- captain-console/venv.sh
+++ captain-console/venv.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
-# set correct python
-command -v python3 >/dev/null 2>&1 &&\
-    py=python3 ||\
-    command -v py3 >/dev/null 2>&1 &&\
-    py=py3
-
-# set correct path based on os
-[[ $OSTYPE =~ "(darwin|linux-gnu)" ]] &&\
-    activatepath=".env/bin/activate" ||\
-    activatepath=".env/scripts/activate"
+# set correct python interpreter and path, based on os
+(command -v python3 >/dev/null 2>&1 &&\
+    { py="sudo python3" ; activatepath=".env/bin/activate" }) ||\
+(command -v py3 >/dev/null 2>&1 &&\
+    { py="py3" ; activatepath=".env/scripts/activate" }) ||\
+{ echo "python 3 required..." ; exit }
 
 # install virtualenv if it doesn't exist
-command -v virtualenv >/dev/null 2>&1 &&\
-    echo "virtualenv installed" ||\
-    sudo $py -m pip install virtualenv
+command -v virtualenv >/dev/null 2>&1 ||\
+    { echo "installing virtualenv...\n" ; $py -m pip install virtualenv }
 
 # create virtual environment, activate and install dependencies
 # if run for first time, else activate it
 stat ".env" >/dev/null 2>&1 &&\
     source $activatepath ||\
-    virtualenv .env; source $activatepath; pip install -r requirements.txt
+    { echo "creating virtualenv...\n" ; virtualenv .env ;\
+        source $activatepath ; pip install -r requirements.txt }
+
+echo "virtualenv activated\n"
 
