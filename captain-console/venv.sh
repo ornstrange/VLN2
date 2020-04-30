285e189079502ee47f322516c1bc76a739a14f7e
ornstrange <orn.strange@gmail.com>
2020-04-30 18:31:50 +0000

    design and FINAL venv.sh script

--- captain-console/venv.sh
+++ captain-console/venv.sh
@@ -1,22 +1,53 @@
 #!/bin/sh
 
-# set correct python interpreter and path, based on os
-(command -v python3 >/dev/null 2>&1 &&\
-    { py="sudo python3" ; activatepath=".env/bin/activate" }) ||\
-(command -v py3 >/dev/null 2>&1 &&\
-    { py="py3" ; activatepath=".env/scripts/activate" }) ||\
-{ echo "python 3 required..." ; exit }
-
-# install virtualenv if it doesn't exist
-command -v virtualenv >/dev/null 2>&1 ||\
-    { echo "installing virtualenv...\n" ; $py -m pip install virtualenv }
-
-# create virtual environment, activate and install dependencies
-# if run for first time, else activate it
-stat ".env" >/dev/null 2>&1 &&\
-    source $activatepath ||\
-    { echo "creating virtualenv...\n" ; virtualenv .env ;\
-        source $activatepath ; pip install -r requirements.txt }
-
-echo "virtualenv activated\n"
+if [[ $OSTYPE =~ "(darwin|linux-gnu)" ]]
+then # macos/linux
+    which python | grep ".env" >/dev/null 2>&1 && return 0
+    if [[ $(python --version | cut -d ' ' -f 2 | cut -d '.' -f 1-2) != "3.8" ]]
+    then
+        echo "python 3.8+ required!\n"
+        return 1
+    fi
+    if [[ ! $(python -c "import virtualenv" >/dev/null 2>&1) ]]
+    then # virtualenv not installed
+        echo "installing virtualenv\n"
+        sudo pip install virtualenv
+    fi
+    if [[ -d ".env" ]]
+    then # env exists
+        echo "activating virtualenv\n"
+        source .env/bin/activate
+    else # create env
+        echo "creating virtualenv\n"
+        virtualenv .env
+        echo "activating virtualenv\n"
+        source .env/bin/activate
+        echo "installing requirements\n"
+        pip install -r requirements.txt
+    fi
+else # windows
+    which py | grep ".env" >/dev/null 2>&1 && return 0
+    if [[ $(py --version | cut -d ' ' -f 2 | cut -d '.' -f 1-2) != "3.8" ]]
+    then
+        echo "python 3.8+ required!\n"
+        return 1
+    fi
+    if [[ ! $(py -c "import virtualenv" >/dev/null 2>&1) ]]
+    then # virtualenv not installed
+        echo "installing virtualenv\n"
+        py -m pip install virtualenv
+    fi
+    if [[ -d ".env" ]]
+    then # env exists
+        echo "activating virtualenv\n"
+        source .env/bin/activate
+    else # create env
+        echo "creating virtualenv\n"
+        py -m virtualenv .env
+        echo "activating virtualenv\n"
+        source .env/bin/activate
+        echo "installing requirements\n"
+        py -m pip install -r requirements.txt
+    fi
+fi
 
