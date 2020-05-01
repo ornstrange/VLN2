5f4b3d92a375804d4dfb65b691efeee18b7199c4
ornstrange <orn.strange@gmail.com>
2020-05-01 23:32:46 +0000

    byrja að setja upp basic html/css

--- captain-console/products/urls.py
+++ captain-console/products/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="Products"),
+    path("offers/", views.offers, name="Offers"),
+    path("consoles/", views.consoles, name="Consoles"),
+    path("games/", views.games, name="Games"),
 ]
 
--- captain-console/products/views.py
+++ captain-console/products/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'products/index.html')
+def offers(request):
+    context = {
+        'title': 'Offers',
+        'data': ["offer 1", "offer 2"]
+    }
+    return render(request, 'products/index.html', context)
 
-def index(request):
-    return render(request, 'products/index.html')
+def consoles(request):
+    context = {
+        'title': 'Consoles',
+        'data': ["console 1", "console 2"]
+    }
+    return render(request, 'products/index.html', context)
 
-def index(request):
-    return render(request, 'products/index.html')
-
-def index(request):
-    return render(request, 'products/index.html')
+def games(request):
+    context = {
+        'title': 'Games',
+        'data': ["game 1", "game 2"]
+    }
+    return render(request, 'products/index.html', context)
 
