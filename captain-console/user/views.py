7a5b651bb238a0aff45fb8c5043791f77a51ecdc
elmarolafss <elmarol250@nemi.tskoli.is>
2020-05-01 18:34:06 +0000

    sma auth stuff

---
+++ captain-console/user/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def register(request):
+    if request.mehthod == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(request, 'user/register.html',{ #eða whatever you want Örn
+        'form' : UserCreationForm()
+    )}
