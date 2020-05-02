242850dde7f3619a7df5c12743a4f15a1f8cbae1
ornstrange <orn.strange@gmail.com>
2020-05-02 17:41:57 +0000

    fixa models

--- captain-console/products/models.py
+++ captain-console/products/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class Product(models.Model):
-    keywords = models.CharField(max_length=50)
-    image = models.CharField(max_length=255)
     name = models.CharField(max_length=50)
-    description = models.CharField(blank=True)
-    categorie = models.CharField(max_length=50)
+    image_count = models.IntegerField()
     price = models.IntegerField()
+    description = models.CharField(blank=True)
+    keywords = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    condition = models.CharField(max_length=50)
+
