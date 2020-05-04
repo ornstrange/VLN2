fff0e18982879d914fd1c18e5c4ca495b76f66c5
elmarolafss <elmarol250@nemi.tskoli.is>
2020-05-04 15:32:17 +0000

    carttabel

--- captain-console/cart/models.py
+++ captain-console/cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from products.models import Product
+from user.models import User
 
 # Create your models here.
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
