10b99553b921ea1c9797b72f937701d7c0510701
elmarolafss <elmarol250@nemi.tskoli.is>
2020-05-06 19:14:31 +0000

    fix

--- captain-console/user/form.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class RegisterForm(forms.Form):
-    username = forms.CharField(label='uname', max_length=100)
-    password = forms.CharField(label='pword',max_length=32, widget=forms.PasswordInput) 
-    cpassword = forms.CharField(label='cpword',max_length=32, widget=forms.PasswordInput) 
--- captain-console/user/views.py
+++ captain-console/user/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect
-from .form import RegisterForm
 from django.contrib.auth.hashers import make_password, check_password
 from user.models import User
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
         data = request.POST.copy()
         username = data.get("uname")
         password = make_password(data.get("pword"))
