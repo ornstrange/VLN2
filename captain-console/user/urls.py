8a3655207ff06728ffd468f612e27dcebd291ad8
Valgeir Ingi <vallzter>
2020-05-08 22:41:58 +0000

    dropdown fyrirlogout

--- captain-console/user/urls.py
+++ captain-console/user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout')
+    
 ]
