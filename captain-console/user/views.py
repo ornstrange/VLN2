56be1ff1392af236565259cee346bb239b7b8cf0
Valgeir Ingi <vallzter>
2020-05-13 20:36:31 +0000

    passw semi komið

--- captain-console/user/views.py
+++ captain-console/user/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from user.forms import SignupForm, EditProfileForm
@@ -87,8 +88,23 @@ def searches(request):
     }
     return render(request, 'user/searches.html', context)
 
+
 def forgotten_view(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  
+            messages.success(request, f'Your password was successfully updated!')
+            return redirect('profile')
+        else:
+            messages.error(request, f'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    
+    return render(request, 'user/forgotten.html', {
+        'form': form
+    })
     context = {
         'style': 'user.css'
     }
-    return render(request, 'user/forgotten.html', context)
